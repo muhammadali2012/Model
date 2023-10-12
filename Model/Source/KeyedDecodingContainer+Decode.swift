b2c6f7f655489252d27fc181f159c43c94a77526
Muhammad Ali <muhammadali@Muhammad-Alis-Mac-Book.local>
2023-10-12 16:10:22 +0500

    Any Value Wrapper Array pending

--- Model/Source/KeyedDecodingContainer+Decode.swift
+++ Model/Source/KeyedDecodingContainer+Decode.swift
@@ -7,7 +7,8 @@
 //
 
 import Foundation
- extension KeyedDecodingContainer {
+// theese functions are called on property wrappers from left to right , left wrapper will be the first.
+extension KeyedDecodingContainer {
     public func decode<P>(_: DefaultValueWrapper<P>.Type, forKey key: Key) throws -> DefaultValueWrapper<P> where P: CodAbleInetilizer {
         if let value = try decodeIfPresent(DefaultValueWrapper<P>.self, forKey: key) {
             return value
@@ -15,18 +16,44 @@ import Foundation
             return DefaultValueWrapper(wrappedValue: P(wrappedValue: P.defaultValue))
         }
     }
-     
-     public func decode<P>(_: AnyValueWrapper<P>.Type, forKey key: Key) throws -> AnyValueWrapper<P> where P: CodAbleInetilizer {
-         if let value = try decodeIfPresent(AnyValueWrapper<P>.self, forKey: key) {
-             return value
-         }
-         else if let value = try decodeIfPresent(P.self, forKey: key) {
-             return AnyValueWrapper(wrappedValue: value)
-         }
-         // check for all types poosible here if any type is filled convert it to inetiate P and used it in AnyValueWrapper and return it 
-         else {
-             return AnyValueWrapper(wrappedValue: P(wrappedValue: P.defaultValue))
-         }
-     }
-     
+    
+    
+    
+    
+    public func decode<P>(_: AnyValueWrapper<P>.Type, forKey key: Key) throws -> AnyValueWrapper<P> where P: CodAbleInetilizer {
+    
+        if let value = try? decodeIfPresent(AnyValueWrapper<P>.self, forKey: key) {
+            return value
+        }
+        // to convert into any type
+        var valueAsString = String()
+        if let valueDouble = try? decodeIfPresent(Double.self, forKey: key) {
+            if let valueInt = valueDouble.toInt() {
+                valueAsString = valueInt.description
+            }else{
+                valueAsString = valueDouble.description
+            }
+        }
+        if let valueBool = try? decodeIfPresent(Bool.self, forKey: key){
+            valueAsString = valueBool.description
+        }
+        if let valueString = try? decodeIfPresent(String.self, forKey: key){
+            valueAsString = valueString
+        }
+        if P.defaultValue is String, let wraped = valueAsString as? P.wrapValueAssociatedType {
+            return AnyValueWrapper(wrappedValue: P(wrappedValue: wraped))
+        }
+        if P.defaultValue is Int, let value: Int = valueAsString.convert(), let wraped = value as? P.wrapValueAssociatedType {
+            return AnyValueWrapper(wrappedValue: P(wrappedValue: wraped))
+        }
+        if P.defaultValue is Double, let value: Double = valueAsString.convert(), let wraped = value as? P.wrapValueAssociatedType {
+            return AnyValueWrapper(wrappedValue: P(wrappedValue: wraped))
+        }
+        if P.defaultValue is Bool, let value: Bool = valueAsString.convert(), let wraped = value as? P.wrapValueAssociatedType {
+            return AnyValueWrapper(wrappedValue: P(wrappedValue: wraped))
+        }
+        
+        return AnyValueWrapper(wrappedValue: P(wrappedValue: P.defaultValue))
+    }
+    
 }
